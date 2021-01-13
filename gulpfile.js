f14fff457bd6f903cd9f457e4fb361342322caa1
dmitriypetukhov <pdnsite@gmail.com>
2021-01-13 17:11:38 +0300

    убрал normalize

--- gulpfile.js
+++ gulpfile.js
@@ -59,7 +59,7 @@ function scripts() {
 
 function styles() {
     return src([
-		'node_modules/normalize.css/normalize.css',
+		// 'node_modules/normalize.css/normalize.css',
 		// 'node_modules/slick-carousel/slick/slick.css',
 		// 'node_modules/magnific-popup/dist/magnific-popup.css',
 		// 'node_modules/rateyo/src/jquery.rateyo.css',
